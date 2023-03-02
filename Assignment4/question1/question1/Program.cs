989375004d8d2d317529f0288a2cd25afee823ef
Elizabeth-L-Mawphlang <72000872+Elizabeth-L-Mawphlang@users.noreply.github.com>
2023-03-02 09:51:14 +0530

    Add files via upload

---
+++ Assignment4/question1/question1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Saledetails sale = new Saledetails(1, 100, 1500, DateTime.Today, 5);
+            Console.Read();
+        }
+        public class Saledetails
+        {
+            private int SalesNo;
+            private int ProductNo;
+            private int Price;
+            private DateTime DateOfSale;
+            private int Qty;
+            private decimal TotalAmount;
+
+            public Saledetails(int salesNo, int productNo, int price, DateTime dateOfSale, int qty)
+            {
+                this.SalesNo = salesNo;
+                this.ProductNo = productNo;
+                this.Price = price;
+                this.DateOfSale = dateOfSale;
+                this.Qty = qty;
+                Sales();
+                ShowData();
+            }
+
+            private void Sales()
+            {
+                this.TotalAmount = Qty * Price;
+            }
+
+            public void ShowData()
+            {
+                Console.WriteLine("\n---------Sales Details---------\n");
+                Console.WriteLine("SalesNo: " + this.SalesNo);
+                Console.WriteLine("ProductNo: " + this.ProductNo);
+                Console.WriteLine("Price: " + this.Price);
+                Console.WriteLine("DateOfSale: " + this.DateOfSale.ToShortDateString());
+                Console.WriteLine("Qty: " + this.Qty);
+                Console.WriteLine("TotalAmount: " + this.TotalAmount);
+            }
+        }
+    }
+}
---
+++ Assignment4/question3/question3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Doctor doctor = new Doctor("John Barley", "Australia", 98766, 1000, "Cardiology");
+           
+        }
+
+
+
+        public class Person
+        {
+            protected string Name;
+            protected string Address;
+
+            public Person(string name, string address)
+            {
+                this.Name = name;
+                this.Address = address;
+            }
+        }
+
+        public class Doctor : Person
+        {
+            private int RegnNo;
+            private int FeesCharged;
+            private string Specialization;
+
+            public Doctor(string name, string address, int regnNo, int feesCharged, string specialization) : base(name, address)
+            {
+                this.RegnNo = regnNo;
+                this.FeesCharged = feesCharged;
+                this.Specialization = specialization;
+            }
+
+        }
+    }
+}
